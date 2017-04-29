79c87d41780a58ae51d9807f73fae800c6c669d5
Jonathan Bjørn Greve <jonagrev@gmail.com>
2017-04-29 23:45:00 +0200

    Added Models to manage the kitchens funds.

---
+++ KitchenIntranetSystem/Models/BankDatas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitchenIntranetSystem.Models
+{
+    public class BankDatas
+    {
+        public int Id { get; set; }
+        public int KitchenAccountId { get; set; }
+        public decimal Total { get; set; }
+        public decimal Debt { get; set; }
+
+        public KitchenAccounts KitchenAccount { get; set; }
+    }
+}
---
+++ KitchenIntranetSystem/Models/KitchenAccounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitchenIntranetSystem.Models
+{
+    public class KitchenAccounts
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } //Owner of the bank account.
+        public string NameOfBank { get; set; } //Eg. Danske Bank, Jyske Bank...
+        public bool? IsActive { get; set; } //Is the bank account still being used.
+
+        public ApplicationUser User { get; set; }
+        public ICollection<BankDatas> BankDatas { get; set; }
+    }
+}
